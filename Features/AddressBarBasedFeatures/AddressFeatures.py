2d8addefee729464564f38702485640d8a6c6254
Sarv06112000 <sarveshpanchal.ssp@gmail.com>
2023-03-25 18:25:26 +0530

    third commit updated AddressFeatures.py file and added Dataset

--- Features/AddressBarBasedFeatures/AddressFeatures.py
+++ Features/AddressBarBasedFeatures/AddressFeatures.py
@@ -1,22 +1,46 @@
+# -1 ----> Phishing
+# 0  ----> Suspicious
+# 1  ----> Legitimate
+
+from Features.Enums import Patterns
+import re
+import ipaddress as ip
+
+
 class AddressFeatures:
     def __init__(self, url):
         self.url = url
         print("hello world")
 
     def usingIPAddress(self, url):
-        return url
+        domain_name = re.findall(Patterns.DOMAIN, self.url)[0]
+        if len(domain_name) > 0:
+            try:
+                if ip.ip_address(domain_name):
+                    return -1
+            except:
+                return 1
 
     def longURL(self, url):
-        return url
+        if len(url) < 54:
+            return 1
+        if 54 <= len(self.url) <= 75:
+            return 0
+        else:
+            return -1
 
     def tinyURL(self, url):
-        return url
+        return self.url
 
     def atRateSymbol(self, url):
-        return url
+        if re.findall('@', self.url):
+            return -1
+        return 1
 
     def redirectDoubleSlash(self, url):
-        return url
+        if self.url.rfind('//') > 6:
+            return -1
+        return 1
 
     def prefixSuffixDomain(self, url):
         return url
